48661191606d58233b5535586de4c0f675d4a6d7
unknown <robin@developereyes.buzz>
2021-03-31 03:17:55 +0530

    Updates

---
+++ precache-manifest.e2bf278c7f89b292df3727ed0f4b4b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "add50c719b8600827e1d",
+    "url": "/test-repo-one/static/css/main.9a3d4813.chunk.css"
+  },
+  {
+    "revision": "add50c719b8600827e1d",
+    "url": "/test-repo-one/static/js/main.add50c71.chunk.js"
+  },
+  {
+    "revision": "5c65df15d8fda55119a3",
+    "url": "/test-repo-one/static/css/1.063b0e9e.chunk.css"
+  },
+  {
+    "revision": "5c65df15d8fda55119a3",
+    "url": "/test-repo-one/static/js/1.5c65df15.chunk.js"
+  },
+  {
+    "revision": "2309be7792a29a56ca62",
+    "url": "/test-repo-one/static/js/runtime~main.2309be77.js"
+  },
+  {
+    "revision": "116de51455f0bbb0bc40e316aae2580b",
+    "url": "/test-repo-one/index.html"
+  }
+];
